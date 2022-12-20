506298bd9f506bb1033fb8a7a061e6cdd0eedc58
jixuan zhi <jixuanzhi@jixuans-MacBook-Pro-2.local>
2022-12-19 23:01:42 -0500

    add server

--- start.sh
+++ start.sh
@@ -1,3 +1,28 @@
-#!/bin/bash
+ID=$(docker ps -qf publish=5001)
+if [ -z $ID ]
+then
+ echo ""
+else 
+ docker stop $ID
+fi
 
-docker run  -p 80:5001 --rm -it -d -v "$(pwd)":/go_source go:1.11 /bin/bash
+while getopts "b:" arg
+do
+   case $arg in
+     b)
+       IS_BUILD=true
+       TAG_NAME=$OPTARG
+       ;;
+     ?)
+       echo "Unknow args $arg... exit..."
+       exit 1
+       ;;
+   esac
+done
+
+if [ "$IS_BUILD" = true ] ; then
+        docker tag go:live go:$TAG_NAME
+	docker build -f ./Dockerfile -t go:live .
+fi
+
+docker run --rm -p 5001:80 -it -d -v "$(pwd)":/go_source go:live go run server.go
