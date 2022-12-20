91c29995f29b14766e4c3e3abd23c0441a9cb290
JixuanZhi <34020442+JixuanZhi@users.noreply.github.com>
2022-12-20 00:00:15 -0500

    Feature/add external calls (#5)
    * add server
    
    * add external call
    
    Co-authored-by: jixuan zhi <jixuanzhi@jixuans-MacBook-Pro-2.local>

--- server.go
+++ server.go
@@ -6,12 +6,19 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-
-    fmt.Fprintf(w, "hello\n")
+    requestURL := "http://google.com"
+    res, err := http.Get(requestURL)
+    if err != nil {
+        fmt.Printf("error making http request: %s\n", err)
+        fmt.Fprintf(w, "call google failed: %s\n", err)
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    fmt.Printf("client: status code: %d\n", res.StatusCode)
+    fmt.Fprintf(w, "%s\n", res)
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
-
     for name, headers := range req.Header {
         for _, h := range headers {
             fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -20,11 +27,7 @@ func headers(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-
-    fmt.Print("1\n")
     http.HandleFunc("/hello", hello)
-    fmt.Print("2\n")
     http.HandleFunc("/headers", headers)
-    fmt.Print("3\n")
     http.ListenAndServe(":80", nil)
 }
