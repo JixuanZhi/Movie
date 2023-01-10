783142818b4be210c5f3fdc95d2c9ba52c2ce7d5
JixuanZhi <34020442+JixuanZhi@users.noreply.github.com>
2023-01-09 23:47:25 -0500

    add docker_compose (#8)
    Co-authored-by: jixuan zhi <jixuanzhi@jixuans-MacBook-Pro-2.local>

---
+++ service_start.sh
@@ -0,0 +1,18 @@
+docker-compose -f deployment.yml down
+
+docker-compose -f deployment.yml rm -f
+
+ENV_FILE=".env"
+rm -f $ENV_FILE
+
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+HOST_IP=`ifconfig | grep 'inet ' | grep -Fv 127.0.0.1 | awk '{print $2}'`
+
+function encode_env {
+  echo "$1=${!1}" >> $ENV_FILE
+}
+
+encode_env "CUR_DIR"
+encode_env "HOST_IP"
+
+docker-compose -f deployment.yml up --build -d
